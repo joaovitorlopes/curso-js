990b53e8eb5e6e6e2e812c4caf54f25a6534856f
joaovitorlopes <jvlaaa+github@gmail.com>
2023-05-19 15:43:06 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log('Olá "mundo"'); // String = Texto
+console.log("Olá 'mundo'");
+console.log(`'Olá' "mundo"`);
+
+console.log(123456);
+console.log(15.85);
+console.log(35, 15.56, 'João Vitor De Novo');
