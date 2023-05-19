39718b55df9907866586e385d18474904607070c
joaovitorlopes <jvlaaa+github@gmail.com>
2023-05-19 16:56:01 -0400

    learning how to declare variables

---
+++ aula-06/index.js
@@ -0,0 +1,18 @@
+// Não se pode criar variáveis com palavras reservadas!!!!
+// Precisam ter nomes significativos!
+// Não se pode começar o nome da variável com um número!
+// Não podem conter espaços ou traços
+// Utiliza-se camelCase
+// Variáveis são case-sensitive!
+let nome; // Declarou a variável
+nome = 'Qualquer valor'; // Inicializando a variável
+console.log(nome);
+nome = 'João';
+console.log(nome);
+
+let pessoa = 'Fulano';
+console.log(pessoa, 'nasceu em 1984.');
+console.log('Em 2000', pessoa, 'conheceu Maria');
+console.log(pessoa, 'casou-se com Maria em 2012.');
+console.log('Maria teve 1 filho com', pessoa, 'em 2015.');
+console.log('O filho de', pessoa, 'se chama Eduardo.');
