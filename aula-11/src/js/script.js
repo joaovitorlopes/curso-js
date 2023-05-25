a7276bb3230185e32d92eef2fc45dea0962bb4ed
João Vitor Lopes <jvlaaa@gmail.com>
2023-05-24 22:15:30 -0400

    alert, confirm and prompt in browser

---
+++ aula-11/src/js/script.js
@@ -0,0 +1,4 @@
+let num1 = parseInt(prompt('Digite um número'));
+let num2 = parseInt(prompt('Digite outro número'));
+const resultado = num1 + num2;
+alert(`O resultado dessa soma foi ${resultado}`); 
