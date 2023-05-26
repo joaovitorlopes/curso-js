f1165104fc620ca8a521e234533f4fca2ecc2f9c
joaovitorlopes <jvlaaa+github@gmail.com>
2023-05-26 14:57:54 -0400

    comparison operators

---
+++ aula-21/script.js
@@ -0,0 +1,15 @@
+/*
+Operadores de comparação
+> maior que
+>= maior que ou igual a
+< menor que
+<= menor que ou igual a
+== igualdade (valor) ****NÃO USAR****
+=== igualdade estrita (valor e tipo)
+!= diferente (valor) ****NÃO USAR****
+!== diferente estrito (valor e tipo)
+*/
+const num1 = 10;
+const num2 = 11;
+const comp = num1 <= num2;
+console.log(comp); // boolean
