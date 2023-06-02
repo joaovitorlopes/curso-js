7a7245cd174946e4f05226e739ff9a45499956d0
joaovitorlopes <jvlaaa+github@gmail.com>
2023-06-02 15:30:06 -0400

    logical exercice 01

---
+++ aula-41/script.js
@@ -0,0 +1,11 @@
+// Escreva uma função que recebe 2 números e retorne o maior deles;
+// function comparaNumeros (num1, num2) {
+//     return num1 > num2 ? num1 : num2;
+// }
+// USANDO ARROW FUNCTION:
+// const max = (x, y) => {
+//     return x > y ? x : y;
+// };
+// ARROW FUNCTION COM APENAS RETORNO
+const max2 = (x, y) => x > y ? x : y;
+console.log(max2(10, 5));
