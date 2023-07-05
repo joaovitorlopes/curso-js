48b137de41e6738bbafb48cb640ff191484fd6b1
joaovitorlopes <jvlaaa+github@gmail.com>
2023-07-05 17:11:46 -0400

    webpack (boilerplate)

---
+++ aula-94/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path'); // CommonJS
+
+//Exportar para ser acessível de fora do arquivo:
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'public', 'assets', 'js'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [{
+            exclude: /node_modules/,
+            test: /\.js$/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/env']
+                }
+            }
+        }]
+    },
+    devtool: 'source-map',
+};
