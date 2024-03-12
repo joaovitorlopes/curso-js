a5f3a50257bdd8dff88d839e431d142bd18c92eb
joaovitorlopes <jvlaaa+github@gmail.com>
2024-03-12 16:11:59 -0400

    express and webpack

---
+++ node/aula-12/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const routes = require('./routes');
+const path = require('path');
+
+app.use(express.urlencoded({ extended: true })); //Handling POST method
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
+app.use(routes);
+
+
+app.listen(5000, () => {
+    console.log('The server has been started in the port 5000');
+    console.log('Acess http://localhost:5000 or http://127.0.0.1:5000');
+});
