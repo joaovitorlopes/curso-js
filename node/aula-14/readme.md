b9b6a46812861eb46d24e363c313242940f68aee
joaovitorlopes <jvlaaa+github@gmail.com>
2024-03-13 13:49:48 -0400

    installation of MongoDB with mongoose and adding a Models

---
+++ node/aula-14/readme.md
@@ -0,0 +1,7 @@
+# installing modules
+- npm install dotenv
+- npm install mongoose
+# using .env
+- create a .env file, an in the file create the vars.
+- call the .env in the main file with "require('dotenv').config()".
+- to use vars of .env file, use "process.env.VARNAME".
---
+++ node/aula-14/server.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.CONNECTIONSTRING)
+    .then(() => {
+        app.emit('ready');
+    })
+    .catch(e => console.log(e));
+
+const routes = require('./routes');
+const path = require('path');
+const { middlewareGlobal } = require('./src/middlewares/middleware');
+
+app.use(express.urlencoded({ extended: true })); //Handling POST method
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
+// Our own middleware
+app.use(middlewareGlobal);
+app.use(routes);
+
+app.on('ready', () => {
+    app.listen(5000, () => {
+        console.log('The server has been started in the port 5000');
+        console.log('Acess http://localhost:5000 or http://127.0.0.1:5000');
+    });
+});
