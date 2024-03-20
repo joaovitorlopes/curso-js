ea5a86db11dfe0294ea69ff8c23022dbcba51148
joaovitorlopes <jvlaaa+github@gmail.com>
2024-03-20 11:19:48 -0400

    creating CRUD methods of users

--- api_rest/src/controllers/UserController.js
+++ api_rest/src/controllers/UserController.js
@@ -4,9 +4,81 @@ class UserController {
   async store(request, response) {
     try {
       const newUser = await User.create(request.body);
-      response.json(newUser);
+      return response.json(newUser);
     } catch (e) {
-      response.status(400).json({
+      return response.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  // Index
+  async index(request, response) {
+    try {
+      const users = await User.findAll();
+      return response.json(users);
+    } catch (e) {
+      return response.json(null);
+    }
+  }
+
+  // Show
+  async show(request, response) {
+    try {
+      const user = await User.findByPk(request.params.id);
+      return response.json(user);
+    } catch (e) {
+      return response.json(null);
+    }
+  }
+
+  // Update
+  async update(request, response) {
+    try {
+      if (!request.params.id) {
+        return response.status(400).json({
+          errors: ['Missing user ID.'],
+        });
+      }
+
+      const user = await User.findByPk(request.params.id);
+
+      if (!user) {
+        return response.status(400).json({
+          errors: ['User not exists.'],
+        });
+      }
+
+      const newData = await user.update(request.body);
+      return response.json(newData);
+    } catch (e) {
+      return response.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  // Delete
+  async delete(request, response) {
+    try {
+      if (!request.params.id) {
+        return response.status(400).json({
+          errors: ['Missing user ID.'],
+        });
+      }
+
+      const user = await User.findByPk(request.params.id);
+
+      if (!user) {
+        return response.status(400).json({
+          errors: ['User not exists.'],
+        });
+      }
+
+      await user.destroy();
+      return response.json(user);
+    } catch (e) {
+      return response.status(400).json({
         errors: e.errors.map((err) => err.message),
       });
     }
